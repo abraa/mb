fe7a7ecbf8225cb3a21e8402290b757f233051eb
胡伟涛 <506711663@qq.com>
2017-08-21 14:06:52 +0800

    上传初始文件

---
+++ application/Common/Controller/InitController.class.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * ====================================
+ * 应用核心控制器
+ * ====================================
+ * Author: 9004396
+ * Date: 2017-08-21 13:54
+ * ====================================
+ * Project: f.gzciji.cn
+ * File: InitController.class.php
+ * ====================================
+ */
+namespace Common\Controller;
+
+use Think\Controller;
+
+class InitController extends Controller
+{
+}
---
+++ application/Common/Model/InitModel.class.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * ====================================
+ *
+ * ====================================
+ * Author: 9004396
+ * Date: 2017-08-21 14:00
+ * ====================================
+ * Project: f.gzciji.cn
+ * File: InitModel.class.php
+ * ====================================
+ */
+namespace Common\Model;
+
+use Think\Model;
+
+class InitModel extends Model{}
