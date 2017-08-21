5e9289ef6ca9dcdba33b256cc19ad13416328bb8
胡伟涛 <506711663@qq.com>
2017-08-21 14:13:19 +0800

    更新代码

--- application/Common/Conf/config.php
+++ application/Common/Conf/config.php
@@ -1,4 +1,24 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+    'MODULE_ALLOW_LIST'     =>  array('Home','Cpanel'),
+    'DEFAULT_MODULE'        =>  'Home',  // 默认模块
+    'DEFAULT_CONTROLLER'    =>  'Index', // 默认控制器名称
+    'DEFAULT_ACTION'        =>  'index', // 默认操作名称
+    'DEFAULT_CHARSET'       =>  'utf-8', // 默认输出编码
+    'DEFAULT_TIMEZONE'      =>  'PRC',  // 默认时区
+
+    'URL_CASE_INSENSITIVE'      => true, //设置URL是否大小写敏感
+    'LOAD_EXT_CONFIG'           => 'mysql,constant', //加载扩展配置
+
+    /*======== 语言配置 ========*/
+    'LANG_SWITCH_ON'            => true,   // 开启语言包功能
+    'DEFAULT_LANG'              => 'zh-cn', // 允许切换的语言列表 用逗号分隔
+
+    'URL_MODEL'                 => 2, //设置URL模式
+    'URL_HTML_SUFFIX'           => 'html|shtml|json',
+
+
+
+    'VIEW_PATH'                 => './../template/Home/',
 );
---
+++ application/Common/Conf/mysql.php
@@ -0,0 +1,17 @@
+<?php
+return array(
+    /* 数据库配置 */
+    'DB_TYPE'   => 'mysql', // 数据库类型
+    'DB_HOST'   => '192.168.148.206', // 服务器地址
+    'DB_NAME'   => 'fgzciji', // 数据库名
+    'DB_USER'   => 'newxianya', // 用户名
+    'DB_PWD'    => '123abc',  // 密码
+    'DB_PORT'   => '3306', // 端口
+
+    //redis 配置
+    'REDIS' => array(
+        'host' => '192.168.148.204',
+        'port' => '6379',
+        'redis_auth' => 'Kg0JCsHaiP1fsoBJDD',
+    ),
+);
