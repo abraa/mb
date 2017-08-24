151dbdc277c16d1434308150491bb4d87d8ac669
胡伟涛 <506711663@qq.com>
2017-08-24 16:21:23 +0800

    修改微信帐号信息

--- application/Common/Conf/constant.php
+++ application/Common/Conf/constant.php
@@ -10,8 +10,8 @@
  * File: constant.php
  * ====================================
  */
-//define('WECHAT_APPID',       'wx883b768f3e2ab0e1');                //应用ID
-//define('WECHAT_APPSECRET',   '2444980a277caf3fbebd36eca2136914');  //应用密钥
+define('WECHAT_APPID',       'wx883b768f3e2ab0e1');                //应用ID
+define('WECHAT_APPSECRET',   '2444980a277caf3fbebd36eca2136914');  //应用密钥
 
-define('WECHAT_APPID',       'wx973e6249cc102c19');                //应用ID
-define('WECHAT_APPSECRET',   'e3cf167db660cbfa961e74160d01a1e2');  //应用密钥
+//define('WECHAT_APPID',       'wx973e6249cc102c19');                //应用ID
+//define('WECHAT_APPSECRET',   'e3cf167db660cbfa961e74160d01a1e2');  //应用密钥
