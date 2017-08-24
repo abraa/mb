5addb432bbd4d3107e3e9fdce7c150e3af777cf5
程赐明 <314588938@qq.com>
2017-08-24 17:02:44 +0800

    优化代码

--- application/Home/Controller/CommentController.class.php
+++ application/Home/Controller/CommentController.class.php
@@ -128,7 +128,7 @@ class CommentController extends Controller {
      */
     private function getOpenid(){
         if(isCheckWechat() === false){
-            //$this->error('请在微信客户端打开链接');
+            $this->error('请在微信客户端打开链接');
         }
 //        $openid = WeChat::getOpenId();
 
